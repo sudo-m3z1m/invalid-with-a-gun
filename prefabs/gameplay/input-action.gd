0c3abba125a00a320ae78f0f1d0257cf2a6bc167
sudo-m3z1m <nizilplay2004@gmail.com>
2025-05-14 23:29:17 +0300

    Added ActionName finding. It so bad, and I need to make it better

--- prefabs/gameplay/input-action.gd
+++ prefabs/gameplay/input-action.gd
@@ -6,9 +6,21 @@ var input_event: InputEvent
 var action_name: String
 
 func _init(event: InputEvent) -> void:
+	action_name = get_action_name(event)
 	input_event = event
-	action_name = event.as_text()
-	#InputMap.get_actions()
 
 func release_action() -> void:
 	pass
+
+func get_action_name(event: InputEvent) -> String:
+	var actions: Array[StringName] = InputMap.get_actions()
+
+	#TODO: Need to do with this algorithm smth
+	#It so slow
+
+	for action_name in actions:
+		if(!event.is_action(action_name)):
+			continue
+		return action_name
+	
+	return ''
